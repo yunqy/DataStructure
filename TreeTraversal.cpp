8cbc270d88e0ea76ea388e9e9e97776ab74fa212
yunchi <yunqyzhang@gmail.com>
2020-12-30 17:05:05 +0800

    Add BSF.

--- TreeTraversal.cpp
+++ TreeTraversal.cpp
@@ -1,3 +1,6 @@
+#include <stack>
+#include <queue>
+#include <iostream>
 template <class T> class Tree;
 
 template <class T>
@@ -15,8 +18,8 @@ public:
 template <class T>
 class Tree {
 public:
-    Tree(void) : root(nullptr) {}
-    Tree(TreeNode<T>* rootNode) : root(rootNode) {}
+    Tree(void);
+    Tree(TreeNode<T>* rootNode);
     void inorder();
     void preorder();
     void postorder();
@@ -25,17 +28,147 @@ private:
     TreeNode<T>* root;
 };
 
-#include <stack>
-#include <queue>
-#include <iostream>
+template <class T>
+class InorderIterator {
+public:
+    InorderIterator(TreeNode<T>* curNode, int count);
+    T* next();
+    bool hasNext();
+private:
+    TreeNode<T>* _curNode;
+    std::stack<TreeNode<T>*> _s;
+    int _count;
+};
+
+template <class T>
+class PreorderIterator {
+public:
+    PreorderIterator(TreeNode<T>* curNode, int count);
+    T* next();
+    bool hasNext();
+private:
+    TreeNode<T>* _curNode;
+    std::stack<TreeNode<T>*> _s;
+    int _count;
+};
+
+template <class T>
+class PostorderIterator {
+public:
+    PostorderIterator(TreeNode<T>* curNode, int count);
+    T* next();
+    bool hasNext();
+private:
+    TreeNode<T>* _curNode;
+    std::stack<TreeNode<T>*> _s;
+    int _count;
+};
+
+// implementation
+
 template <class T>
 TreeNode<T>::TreeNode(T data): _data(data), _traveled(false), _leftChild(nullptr), _rightChild(nullptr) {}
+
+template <class T>
+InorderIterator<T>::InorderIterator(TreeNode<T>* curNode, int count) : _curNode(curNode), _count(count) {}
+
+template <class T>
+PreorderIterator<T>::PreorderIterator(TreeNode<T>* curNode, int count) : _curNode(curNode), _count(count) {}
+
+template <class T>
+PostorderIterator<T>::PostorderIterator(TreeNode<T>* curNode, int count) : _curNode(curNode), _count(count) {}
+
+template <class T>
+Tree<T>::Tree(void) : root(nullptr) {}
+
+template <class T>
+Tree<T>::Tree(TreeNode<T>* rootNode) : root(rootNode) {}
 
 template <class T>
 void TreeNode<T>::visit() {
     _traveled = true;
     std::cout << " " << _data;
 };
+
+template <class T>
+T* InorderIterator<T>::next() {
+    while(_curNode) {
+        _s.push(_curNode);
+        _curNode = _curNode->_leftChild;
+    }
+    if (_s.empty()) return nullptr;
+    _curNode = _s.top();
+    _s.pop();
+    T* returnVal = &_curNode->_data;
+    _curNode = _curNode->_rightChild;
+    --_count;
+    return returnVal;
+}
+
+template <class T>
+bool InorderIterator<T>::hasNext() {
+    if(_count > 0) return true;
+    else return false;
+}
+
+template <class T>
+T* PreorderIterator<T>::next() {
+    T* returnVal;
+    while(1) {
+        if (_curNode) {
+            returnVal = &_curNode->_data;
+            _s.push(_curNode);
+            _curNode = _curNode->_leftChild;
+            break;
+        } else {
+            if (_s.empty()) break;
+            _curNode = _s.top();
+            _s.pop();
+            if (_curNode->_rightChild != nullptr) {
+                _curNode = _curNode->_rightChild;
+            } else {
+                _curNode = nullptr;
+            }
+        }
+    }
+    --_count;
+    return returnVal;
+}
+
+template <class T>
+bool PreorderIterator<T>::hasNext() {
+    if(_count > 0) return true;
+    else return false;
+}
+
+template <class T>
+T* PostorderIterator<T>::next() {
+    T* returnVal;
+    while(1) {
+        while (_curNode) {
+            _s.push(_curNode);
+            _s.push(_curNode);
+            _curNode = _curNode->_leftChild;
+        }
+        if (_s.empty()) break;
+        _curNode = _s.top();
+        _s.pop();
+        if (!_s.empty() && _s.top() == _curNode) _curNode = _curNode->_rightChild;
+        else {
+            returnVal = &_curNode->_data;
+            _curNode = nullptr;
+            break;
+        }
+    }
+    --_count;
+    return returnVal;
+}
+
+template <class T>
+bool PostorderIterator<T>::hasNext() {
+    if(_count > 0) return true;
+    else return false;
+}
 
 template <class T>
 void Tree<T>::inorder() {
@@ -135,4 +268,24 @@ int main() {
     tree.preorder();
     tree.postorder();
     tree.bfs();
+    InorderIterator<int> iit(root, 7);
+    std::cout << "Inorder Iterator: [ ";
+    while(iit.hasNext()) {
+        std::cout << *iit.next() << " ";
+    }
+    std::cout << "]" << std::endl;
+
+    PreorderIterator<int> preit(root, 7);
+    std::cout << "Preorder Iterator: [ ";
+    while(preit.hasNext()) {
+        std::cout << *preit.next() << " ";
+    }
+    std::cout << "]" << std::endl;
+
+    PostorderIterator<int> postit(root, 7);
+    std::cout << "Postorder Iterator: [ ";
+    while(postit.hasNext()) {
+        std::cout << *postit.next() << " ";
+    }
+    std::cout << "]" << std::endl;
 }
